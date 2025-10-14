cddc209a543d522384ad1afc16de38c5cbb914ae
sergio daniel <sguzmansalas217@gmail.com>
2025-10-14 10:19:43 -0600

    Primer commit del proyecto API-Calendar-WH

---
+++ googleAuth.js
@@ -0,0 +1,19 @@
+import { google } from 'googleapis';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+
+oauth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+
+async function testCalendar() {
+  const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+  const res = await calendar.events.list({ calendarId: 'primary', maxResults: 5 });
+  console.log(res.data.items);
+}
+
+testCalendar();
